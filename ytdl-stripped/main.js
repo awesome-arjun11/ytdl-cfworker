1e605bb2209273bf26c52d233ef5a115cda93020
Arjun Singh <awesome-arjun11@github.com>
2020-07-11 22:29:40 +0530

    +ytdl-core link

--- ytdl-stripped/main.js
+++ ytdl-stripped/main.js
@@ -1,5 +1,5 @@
 /** 
- * stripped version of ytdl core to reduce size and resolve issues with cloudflare and webpack
+ * stripped version of ytdl-core (https://github.com/fent/node-ytdl-core) to reduce size and resolve issues with cloudflare and webpack
  * Basically ytdl-core without actual video downloading part and with the fetch API.
 */
 
